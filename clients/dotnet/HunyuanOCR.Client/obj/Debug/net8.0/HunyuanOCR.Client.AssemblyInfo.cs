7a29a40da2dc952f069d7ec6af60cef596c8fc66
go3212 <go3212@hotmail.com>
2026-02-06 01:17:56 +0100

    dawd

--- clients/dotnet/HunyuanOCR.Client/obj/Debug/net8.0/HunyuanOCR.Client.AssemblyInfo.cs
+++ clients/dotnet/HunyuanOCR.Client/obj/Debug/net8.0/HunyuanOCR.Client.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute((".NET client for HunyuanOCR API - supports async and sync operations with batch pr" +
     "ocessing"))]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0+61106104b9130ed9b9e6a7aecd4796e07b6464dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0+f3270f75551345aae5628afe6916478f84a8caa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HunyuanOCR.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HunyuanOCR.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.1.0.0")]
